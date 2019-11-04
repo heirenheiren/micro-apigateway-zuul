75635f8f7a958645f45f0dc239e4c51fa801eaf2
Polin <853450031@qq.com>
2019-11-04 23:34:42 +0800

    20191104Zuul网关

---
+++ src/main/java/com/micro/apigateway/zuul/MicroApiGatewayZuulApplication.java
@@ -0,0 +1,21 @@
+package com.micro.apigateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class MicroApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApiGatewayZuulApplication.class, args);
+	}
+
+}
