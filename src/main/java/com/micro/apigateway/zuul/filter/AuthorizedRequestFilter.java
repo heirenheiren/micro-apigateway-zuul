afca9882b656418b585ee9d56b12ba229858160b
Polin <853450031@qq.com>
2019-11-07 19:04:08 +0800

    update

--- src/main/java/com/micro/apigateway/zuul/filter/AuthorizedRequestFilter.java
+++ src/main/java/com/micro/apigateway/zuul/filter/AuthorizedRequestFilter.java
@@ -1,5 +1,8 @@
 package com.micro.apigateway.zuul.filter;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,10 +14,10 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
 /**
- * 拦截指定url, 进行参数校验
+ * 用户登录校验Filter
  */
 @Component
-public class UrlFilter extends ZuulFilter{
+public class AuthorizedRequestFilter extends ZuulFilter{
 	//    private String pre_filterType = PRE_TYPE;    // 前置过滤器(url之前执行)
 //  private String post_filterType = POST_TYPE;  // 后置过滤器
 //  private String error_filterType = ERROR_TYPE;// 异常过滤器
@@ -24,16 +27,22 @@ public class UrlFilter extends ZuulFilter{
    */
   @Override
   public String filterType() {
+	// 在进行Zuul过滤的时候可以设置其过滤执行的位置，那么此时有如下几种类型：
+      // 1、pre：在请求发出之前执行过滤，如果要进行访问，肯定在请求前设置头信息
+      // 2、route：在进行路由请求的时候被调用；
+      // 3、post：在路由之后发送请求信息的时候被调用；
+      // 4、error：出现错误之后进行调用
       return "Pre";
   }
   /**
    * 过滤器顺序(多个过滤器时, 越小的越先执行)
+   * 设置优先级，数字越大优先级越低
    * @return
    */
   @Override
   public int filterOrder() {
 
-      return 3;
+      return 0;
   }
   /**
    * 过滤器是否生效
@@ -47,14 +56,14 @@ public class UrlFilter extends ZuulFilter{
 
       // System.out.println(request.getRequestURI());
       // System.out.println(request.getRequestURL());
-
-      if ("/apigateway/order/api/v1/order/find".equalsIgnoreCase(request.getRequestURI())){
-          return true;
-      }
-      return false;
+//拦截指定url, 进行参数校验
+//      if ("/v1/micro-service/study/getStudy".equalsIgnoreCase(request.getRequestURI())){
+//          return true;
+//      }
+      return true;
   }
   /**
-   * 业务逻辑
+   * 业务逻辑:拿到用户名和密码请求数据库判断是否存在
    * @return
    * @throws ZuulException
    */
@@ -76,6 +85,13 @@ public class UrlFilter extends ZuulFilter{
           requestContext.setSendZuulResponse(false);
           requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
       }
+      
+      String auth = "studyjava:hello"; // 认证的原始信息
+      byte[] encodedAuth = Base64.getEncoder()
+              .encode(auth.getBytes(Charset.forName("US-ASCII"))); // 进行一个加密的处理
+      // 在进行授权的头信息内容配置的时候加密的信息一定要与“Basic”之间有一个空格
+      String authHeader = "Basic " + new String(encodedAuth);
+      requestContext.addZuulRequestHeader("Authorization", authHeader);
       return null;
   }
 }
